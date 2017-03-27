5b475d51b325f880b8c89bd5303382be75eb6794
krubottom <karl.rubottom@gmail.com>
2017-03-26 20:17:15 -0600

    Updates

--- passcheck.py
+++ passcheck.py
@@ -4,9 +4,11 @@ from string import ascii_letters
 
 keywords = [''.join(i) for i in product(ascii_letters, repeat = 3)]
 
-print len(keywords)
+# print len(keywords)
 
-# passbase = raw_input("Please enter the base password: ")
-#
-# for prefix in keywords:
-#     print prefix + passbase
+passbase = raw_input("Please enter the base password: ")
+
+for prefix in keywords:
+    print prefix + passbase
+
+print "Generated " + str(len(keywords)) + " passwords"
